be7b97d91fa6edb854a0f24a098dc5879b42d1fd
kelvin-fernandes <sokelvinfernandes@gmail.com>
2020-06-11 21:16:45 -0300

    Implemented update repository endpoint

--- src/app.js
+++ src/app.js
@@ -31,7 +31,28 @@ app.post("/repositories", (request, response) => {
 });
 
 app.put("/repositories/:id", (request, response) => {
-    // TODO
+    const { id } = request.params;
+    const { title, url, techs } = request.body;
+
+    const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+    if (repositoryIndex < 0) {
+        return response.status(400).json({ error: "Repository not found." });
+    }
+
+    const repository = repositories[repositoryIndex];
+
+    const updatedRepository = {
+        id: repository.id,
+        title,
+        url,
+        techs,
+        likes: repository.likes
+    };
+
+    repositories[repositoryIndex] = updatedRepository;
+
+    return response.json(updatedRepository);
 });
 
 app.delete("/repositories/:id", (request, response) => {
